52dcf5661b21fd474338280a5a31e764aa215577
Steve Grard <stevegrard1@gmail.com>
2023-04-20 18:23:32 +0200

    Global ergonomy in progress

---
+++ assets/javascript/show_purchases.js
@@ -0,0 +1,8 @@
+function showPurchases() {
+    const purchases = document.querySelector("shopping_cart_infos");
+  
+    // toggle the render
+    purchases.classList.toggle("renderPurchases");
+    console.log(purchases);
+  }
+  
