565a1d9e1997dae52ff664ae2920369ebdc48875
JodieAddis <jodie.addis.92@gmail.com>
2023-04-21 08:55:50 +0200

    Continuation...

--- assets/javascript/filter.js
+++ assets/javascript/filter.js
@@ -5,19 +5,18 @@ function filter(category){
         const filter_dish = menu.filter(menu => menu.category === category);
         selected_dish.push(filter_dish);
     })
-    let vegan = category.includes('vegan') ;
-    if (vegan == true){
-        // .style.display = 'block';
-    } else {
-        // .style.display = 'none';
-    }
+    // let vegan = category.includes('vegan') ;
+    // if (vegan == true){
+    //     // .style.display = 'block';
+    // } else {
+    //     // .style.display = 'none';
+    // }
     return selected_dish;
 }
-console.log(filter); 
 
-// console.log(filter('vegan')); 
-// console.log(filter('comfortfood'));
-// console.log(filter('traditionalfood')); 
+console.log(filter('vegan')); 
+console.log(filter('comfortfood'));
+console.log(filter('traditionalfood')); 
 
 
 // console.log(test("vegan"));
